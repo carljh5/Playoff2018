753ce8477f1f63092ff3087b563b6ee2545b3672
Unknown <themunk@live.dk>
2018-08-28 18:42:52 +0200

    temp save

---
+++ Stanhaftig/Assets/SpeechScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechScript : MonoBehaviour
+{
+    public GameObject SpeechBubble;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpeechBubble.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SpeechBubble.SetActive(true);
+        }
+    }
+}
