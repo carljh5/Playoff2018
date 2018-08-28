43b8c94df091fee833a6bf7e08eb783a06ab6ba5
Carl <carl@carljh.dk>
2018-08-28 16:44:07 +0200

    hit and hurt animations and controller

---
+++ Stanhaftig/Assets/RagdollAnimator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RagdollAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+    [Serializable]
+    public class Limb {
+        public string name = "limb";
+        public SpriteRenderer[] sprites;
+    }
+
+    public List<Limb> detachableLimbs = new List<Limb>(); 
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    public void Hurt() {
+        animator.SetTrigger("Hurt");
+    }
+
+    public void Hit() {
+        animator.SetTrigger("Hit");
+    }
+
+    public void DetachLimb(Limb limb) {
+
+
+    }
+	
+	
+	void Update () {
+        //if (Input.GetKeyDown(KeyCode.A))
+        //    Hurt();
+        //if (Input.GetKeyDown(KeyCode.B))
+            //Hit();
+	}
+}
