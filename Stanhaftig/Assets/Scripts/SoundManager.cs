2ddba43ff23017b41065a9a9837a7065f2ecf6cd
Unknown <themunk@live.dk>
2018-08-28 16:41:51 +0200

    Updated and prefabbed Soundmanager + Added Sounds

--- Stanhaftig/Assets/Scripts/SoundManager.cs
+++ Stanhaftig/Assets/Scripts/SoundManager.cs
@@ -7,6 +7,10 @@ public class SoundManager : MonoBehaviour {
     public AudioSource BackgroundAudio;
     private static SoundManager instance;
     public AudioClip[] HitAudios;
+    public AudioClip[] BloodAudios;
+    public AudioClip[] YellAudios;
+    public AudioClip[] SpeechAudios;
+    public AudioClip WinMusic;
 
 
     private void Start()
@@ -17,8 +21,32 @@ public class SoundManager : MonoBehaviour {
     
     public static void PlayHit()
     {
-        if(instance.HitAudios.Length > 0 && instance.FxAudio)
+        if (instance.HitAudios.Length > 0 && instance.FxAudio)
             instance.FxAudio.PlayOneShot(instance.HitAudios[Random.Range(0, instance.HitAudios.Length)]);
+    }
+
+    public static void PlayBlood()
+    {
+        if (instance.BloodAudios.Length > 0 && instance.FxAudio)
+            instance.FxAudio.PlayOneShot(instance.BloodAudios[Random.Range(0, instance.BloodAudios.Length)]);
+    }
+
+    public static void PlayYell()
+    {
+        if (instance.YellAudios.Length > 0 && instance.FxAudio)
+            instance.FxAudio.PlayOneShot(instance.YellAudios[Random.Range(0, instance.YellAudios.Length)]);
+    }
+
+    public static void PlaySpeech()
+    {
+        if (instance.SpeechAudios.Length > 0 && instance.FxAudio)
+            instance.FxAudio.PlayOneShot(instance.SpeechAudios[Random.Range(0, instance.SpeechAudios.Length)]);
+    }
+
+    public static void PlayWin()
+    {
+        if (instance.WinMusic && instance.FxAudio)
+            instance.FxAudio.PlayOneShot(instance.WinMusic);
     }
 
 }
