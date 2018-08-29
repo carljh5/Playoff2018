06eec804fd07af217890d421b5556a1c35e41427
Carl <carl@carljh.dk>
2018-08-29 11:56:31 +0200

    Improved visualisation of taunts

--- Stanhaftig/Assets/terminateOnTime.cs
+++ Stanhaftig/Assets/terminateOnTime.cs
@@ -1,19 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class terminateOnTime : MonoBehaviour {
+    public Transform playerHead;
     
-    
+    TextMeshPro textMesh;
 
-	void OnEnable()
+    private void Start()
     {
+        if (textMesh == null)
+            textMesh = GetComponent<TextMeshPro>();
+    }
+
+    void OnEnable()
+    {
+        if(textMesh == null)
+            textMesh = GetComponent<TextMeshPro>();
         StartCoroutine(waitForSeconds());
+        transform.position = playerHead.position + (Vector3.up * 1.5f);
     }
 
     IEnumerator waitForSeconds()
     {
-        yield return new WaitForSeconds(3f);
+        float time = 0;
+        float fadeDuration = 0.25f;
+        float alpha = 0f;
+        while (time < fadeDuration) {
+            alpha = Mathf.Lerp(0, 1, time / fadeDuration);
+            textMesh.alpha = alpha;
+            time += Time.deltaTime;
+            yield return null;
+        }
+        alpha = 1f;
+        textMesh.alpha = alpha;
+        time = 0;
+        yield return new WaitForSeconds(2f);
+        while (time < fadeDuration)
+        {
+            alpha = Mathf.Lerp(1, 0, time / fadeDuration);
+            textMesh.alpha = alpha;
+            time += Time.deltaTime;
+            yield return null;
+        }
+        alpha = 0f;
+        textMesh.alpha = alpha;
         gameObject.SetActive(false);
     }
+
+    private void Update()
+    {
+        Vector3 curVelocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, (playerHead.position + (Vector3.up * 1.5f)), ref curVelocity, 0.1f);
+    }
+
 }
