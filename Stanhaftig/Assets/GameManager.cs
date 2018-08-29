6f7f2231a1727d41a1fdfa77f065d8c2c8fad6be
Paw Høvsgaard <pawhovsgaard@gmail.com>
2018-08-29 20:09:29 +0200

    small fixes

--- Stanhaftig/Assets/GameManager.cs
+++ Stanhaftig/Assets/GameManager.cs
@@ -82,7 +82,8 @@ public class GameManager : MonoBehaviour {
 
     void Update()
     {
-        StartGame();
+        if(!GameStarted())
+            StartGame();
 
         if (Input.GetKeyDown(RestartKey))
             SceneManager.LoadScene(Application.loadedLevel);
@@ -94,13 +95,15 @@ public class GameManager : MonoBehaviour {
         if (Input.GetKeyDown(Player1.Movement.Jump))
         {
             Player1.gameObject.SetActive(true);
-            Player1.KeyHintCanvas.SetActive(false);
+            if(Player1.KeyHintCanvas)
+                Player1.KeyHintCanvas.SetActive(false);
         }
 
         if (Input.GetKeyDown(Player2.Movement.Jump))
         {
             Player2.gameObject.SetActive(true);
-            Player2.KeyHintCanvas.SetActive(false);
+            Player1.KeyHintCanvas.SetActive(false);
+                Player2.KeyHintCanvas.SetActive(false);
         }
     }
 
