f0a835eba0aed613e6f560c22b1d4d3bfe9e1184
Carl <carl@carljh.dk>
2018-08-30 03:27:16 +0200

    bucket sound on anim

---
+++ Stanhaftig/Assets/PlaySoundAnimEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundAnimEvent : MonoBehaviour {
+
+    public void PlaySound() {
+        SoundManager.PlayBucket();
+    }
+}
